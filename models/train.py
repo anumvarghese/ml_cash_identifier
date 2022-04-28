a63986dfa1e09a4184e1a9057a8aedffd448b025
Anu Mathew <anu.varghese1786@gmail.com>
2022-04-28 07:39:37 +0100

    Created pipeline.py and adding production deployment code

---
+++ models/train.py
@@ -0,0 +1,145 @@
+
+import matplotlib.pylab as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+import os
+import fire
+
+# Load compressed models from tensorflow_hub
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+
+CLASS_NAMES = ['10', '100', '20', '200', '5', '50', '500']
+train_data_path = "gs://team4-cash-classifyimgs/euro/train_set.csv"
+eval_data_path = "gs://team4-cash-classify/imgs/euro/eval_set.csv"
+
+IMG_HEIGHT = 224
+IMG_WIDTH = 224
+IMG_CHANNELS = 3
+
+
+def training_plot(metrics, history):
+  f, ax = plt.subplots(1, len(metrics), figsize=(5*len(metrics), 5))
+  for idx, metric in enumerate(metrics):
+    ax[idx].plot(history.history[metric], ls='dashed')
+    ax[idx].set_xlabel("Epochs")
+    ax[idx].set_ylabel(metric)
+    ax[idx].plot(history.history['val_' + metric]);
+    ax[idx].legend([metric, 'val_' + metric])
+
+# Call model.predict() on a few images in the evaluation dataset
+def plot_predictions(model, filename):
+  f, ax = plt.subplots(7, 5, figsize=(25,15))
+  dataset = (tf.data.TextLineDataset(filename).
+      map(decode_csv))
+  for idx, (img, label) in enumerate(dataset.take(35)):
+    ax[idx//5, idx%5].imshow((img.numpy()));
+    batch_image = tf.reshape(img, [1, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS])
+    batch_pred = model.predict(batch_image)
+    pred = batch_pred[0]
+    label = CLASS_NAMES[label.numpy()]
+    pred_label_index = tf.math.argmax(pred).numpy()
+    pred_label = CLASS_NAMES[pred_label_index]
+    prob = pred[pred_label_index]
+    ax[idx//5, idx%5].set_title('{}: {} ({:.4f})'.format(label, pred_label, prob))
+    ax[idx//5, idx%5].axis('off')
+
+def show_trained_weights(model):
+  # CLASS_NAMES is ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+  LAYER = 1 # Layer 0 flattens the image, layer=1 is the first dense layer
+  WEIGHT_TYPE = 0 # 0 for weight, 1 for bias
+
+  f, ax = plt.subplots(1, 5, figsize=(15,15))
+  for flower in range(len(CLASS_NAMES)):
+    weights = model.layers[LAYER].get_weights()[WEIGHT_TYPE][:, flower]
+    min_wt = tf.math.reduce_min(weights).numpy()
+    max_wt = tf.math.reduce_max(weights).numpy()
+    flower_name = CLASS_NAMES[flower]
+    print("Scaling weights for {} in {} to {}".format(
+        flower_name, min_wt, max_wt))
+    weights = (weights - min_wt)/(max_wt - min_wt)
+    ax[flower].imshow(weights.reshape(IMG_HEIGHT, IMG_WIDTH, 3));
+    ax[flower].set_title(flower_name);a
+    ax[flower].axis('off')
+
+
+def read_and_decode(filename, reshape_dims):
+  # Read the file
+  img = tf.io.read_file(filename)
+  # Convert the compressed string to a 3D uint8 tensor.
+  img = tf.image.decode_jpeg(img, channels=IMG_CHANNELS)
+  # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+  img = tf.image.convert_image_dtype(img, tf.float32)
+  # Resize the image to the desired size.
+  return tf.image.resize(img, reshape_dims)
+
+
+
+# the label is the index into CLASS_NAMES array
+def decode_csv(csv_row):
+  record_defaults = ["path", "flower"]
+  filename, label_string = tf.io.decode_csv(csv_row, record_defaults)
+  img = read_and_decode(filename, [IMG_HEIGHT, IMG_WIDTH])
+  label = tf.argmax(tf.math.equal(CLASS_NAMES, label_string))
+  return img, label
+
+# parameterize to the values in the previous cell
+def train_and_evaluate(train_data_path=f'gs://team4-cash-classify/imgs/euro/train_set.csv',
+                       eval_data_path=f'gs://team4-cash-classify/imgs/euro/eval_set.csv',
+                       CLASS_NAMES = ['10', '100', '20' ,'200', '5', '50', '500'], 
+                       batch_size = 32,
+                       lrate = 0.001,
+                       l1 = 0.,
+                       l2 = 0.,
+                       num_hidden = 16):
+  regularizer = tf.keras.regularizers.l1_l2(l1, l2)
+
+  train_dataset = (tf.data.TextLineDataset(
+      train_data_path).
+      map(decode_csv)).batch(batch_size)
+
+  eval_dataset = (tf.data.TextLineDataset(
+      eval_data_path).
+      map(decode_csv)).batch(32) # this doesn't matter
+
+  layers = [
+     
+      tf.keras.layers.RandomFlip(),
+      tf.keras.layers.RandomRotation(0.1),
+      hub.KerasLayer(
+          "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4",
+          input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS),
+          trainable=False,
+          name='mobilenet_embedding'),
+
+      tf.keras.layers.Dense(num_hidden,
+                            kernel_regularizer=regularizer, 
+                            activation='relu',
+                            name='dense_hidden'),
+      tf.keras.layers.Dense(len(CLASS_NAMES), 
+                            kernel_regularizer=regularizer,
+                            activation='softmax',
+                            name='flower_prob')
+  ]
+
+  model = tf.keras.Sequential(layers, name='flower_classification')
+  model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lrate),
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                    from_logits=False),
+                metrics=['accuracy'])
+  
+  history = model.fit(train_dataset, validation_data=eval_dataset, epochs=10)    
+  # Save the model    
+  #model_filename = "model.pkl"
+  #with open(model_filename, "wb") as model_file:
+  #    pickle.dump(history, model_file)
+  #gcs_model_path = f"gs://team4-cash-classify/{model_filename}"
+  #subprocess.check_call(
+  #   ["gsutil", "cp", model_filename, gcs_model_path], stderr=sys.stdout
+  #)
+  gcs_model_path = f"gs://team4-cash-classify/cash-classify-model-v1"  
+  #model.save(os.getenv('AIP_MODEL_DIR'))
+  #model.save(gcs_model_path)
+  return model
+
+
